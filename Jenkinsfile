68a27a21fdddbe9657830877b39cbec7b50a9d98
Priyanko Sarkar <priyanko.sarkar1994@gmail.com>
2022-04-24 16:14:05 +0530

    pipeline script added

---
+++ Jenkinsfile
@@ -0,0 +1,52 @@
+pipeline {
+    agent any
+
+    environment{
+        DOCKERHUB_CREDENTIALS=credentials('dockerCred')
+    }
+
+    stages {
+        stage('Clone project') {
+            steps {
+                git 'https://github.com/PRIYANKO-21/CovidQuarantineCentreManagementPortal.git'
+                echo 'Project is cloned successfully'
+            }
+        }
+        stage('Build'){
+            steps {
+                sh 'mvn clean install'
+                echo 'Maven build completed'
+            }
+        }
+        stage('Test'){
+            steps {
+                sh 'mvn test'
+                echo 'Testing completed'
+            }
+        }
+        stage('Build image'){
+             steps {
+                echo 'creating docker  image'
+                sh 'docker build -t CQCMP:latest .'
+                echo 'docker image created'
+            }
+        }
+        stage('Push image to docker hub'){
+            steps {
+                echo 'docker tag'
+                sh 'docker tag cqcmp priyanko27/CQCMP:latest'
+                echo 'docker login'
+                sh 'echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin'
+                echo 'Pushing image to hub'
+                sh 'docker push priyanko27/CQCMP'
+                echo 'docker logout'
+                sh 'docker logout'
+            }
+        }
+        stage('Pull image from docker hub'){
+            steps {
+                ansiblePlaybook colorized: true, disableHostKeyChecking: true, installation: 'Ansible', inventory: 'inventory', playbook: 'playbook.yml'
+            }
+        }
+    }
+}
---
+++ src/test/java/com/CQCMP/CQCMP/CQCMPTestService.java
@@ -0,0 +1,221 @@
+package com.CQCMP.CQCMP;
+
+import com.CQCMP.CQCMP.Dto.*;
+import com.CQCMP.CQCMP.Services.CQCMPService;
+import com.CQCMP.CQCMP.Services.JwtService;
+import com.CQCMP.CQCMP.entity.*;
+import com.CQCMP.CQCMP.repo.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.List;
+
+
+public class CQCMPTestService {
+
+    //@Mock
+    private adminLogin_repo adminlogin_repo;
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private addTest_repo addtest_repo;
+
+    @Autowired
+    private addRoom_repo addroom_repo;
+
+    @Autowired
+    private AddMember_repo addMember_repo;
+
+    @Autowired
+    private GetRooms_repo getRooms_repo;
+
+    @Autowired
+    private allocateRoom_repo allocateroom_repo;
+
+    CQCMPService service=new CQCMPService();
+
+    //private Admin
+
+    @BeforeEach
+    public void setup()
+    {
+        adminlogin_repo=Mockito.mock(adminLogin_repo.class);
+        ReflectionTestUtils.setField(service,"adminlogin_repo",adminlogin_repo);
+    }
+    @org.junit.jupiter.api.Test
+    public void testGetAdminById(){
+        //CQCMPService service=new CQCMPService();
+        Admin admin=new Admin();
+        admin.setName("admin");
+        Mockito.when(adminlogin_repo.getAdminById(ArgumentMatchers.anyString())).thenReturn(admin);
+        Admin res= service.getAdminById("7676");
+        Assertions.assertEquals("admin",res.getName());
+        /*String msg;
+        if(res==null){
+            msg="Wrong credentials";
+        }
+        else{
+            msg="Login successfull";
+        }
+        System.out.println(msg);*/
+    }
+
+    public long generateID(){
+        long id=(long) Math.floor(Math.random()*9_000_000_000L)+1_000_000_000L;
+        return id;
+    }
+
+    //@Test
+    public String loginAdmin(AuthRequest authRequest){
+        Admin admin=adminlogin_repo.getAdminByEmail(authRequest.getUsername());
+        if(admin!=null){
+            boolean isMatch=authRequest.getPassword().equals(admin.getPassword());
+            if(isMatch){
+                String token=jwtService.createToken(admin.getAdminID());
+                return token;
+            }
+            else{
+                return null;
+            }
+        }
+        else{
+            return null;
+        }
+    }
+
+    //Test
+    public String addRoom(AddRoomDto addroomDto1){
+
+        int roomNum = addroomDto1.getRoomNum();
+        int floorNum=addroomDto1.getFloorNum();
+        //We have to check whether the admin is adding or someone else is adding the data
+        Rooms room=addroom_repo.getRoomsByRoomAndFloor(roomNum,floorNum);
+        if(room==null){
+            Rooms room1 = new Rooms();
+            room1.setRoomNum(addroomDto1.getRoomNum());
+            room1.setFloorNum(addroomDto1.getFloorNum());
+            room1.setFreeRoom(addroomDto1.getFreeRoom());
+            room1.setFreeRoom(1);
+            long id=generateID();
+            room1.setRoom_id(String.valueOf(id));
+            addroom_repo.save(room1);
+
+            return "Room added successfully";
+        }
+        else{
+            return null;
+        }
+
+    }
+
+    public String addMember(AddMember addMember1){
+
+        String email = addMember1.getSEmail();
+
+        Students student=addMember_repo.getStudentByEmail(email,addMember1.getSRoll(),addMember1.getSContact());
+        if(student==null){
+            Students std = new Students();
+            std.setContact(addMember1.getSContact());
+            std.setEmail_id(addMember1.getSEmail());
+            std.setStudentName(addMember1.getSName());
+            std.setRollNum(addMember1.getSRoll());
+            long id=generateID();
+            std.setStudent_id(String.valueOf(id));
+            addMember_repo.save(std);
+
+            return "Student added successfully";
+        }
+        else{
+            return null;
+        }
+
+    }
+
+    public String addTest(AddTestDto addtestDto1){
+
+        String student_id = addtestDto1.getStudent_id();
+
+        //We have to check whether the admin is adding or someone else is adding the data
+        Test test=addtest_repo.getTestByStudent(student_id);
+        if(test==null){
+            //System.out.println(room);
+            Test test1 = new Test();
+            test1.setStudent_id(addtestDto1.getStudent_id());
+            test1.setResult(addtestDto1.getResult());
+            long id=generateID();
+            test1.setTest_id(String.valueOf(id));
+            addtest_repo.save(test1);
+
+            return "Test record added successfully";
+        }
+        else{
+            return null;
+        }
+
+    }
+
+    public String allocateRoom(AllocateRoomDto allocateRoomDto){
+
+        String student_id = allocateRoomDto.getStudent_id();
+        String room_id=allocateRoomDto.getRoom_id();
+        //We have to check whether the admin is adding or someone else is adding the data
+        Allocation alloc=allocateroom_repo.getExistingAllocation(room_id,student_id);
+        if(alloc==null){
+            //System.out.println(room);
+            Allocation alloc1 = new Allocation();
+            alloc1.setStudent_id(allocateRoomDto.getStudent_id());
+            alloc1.setRoom_id(allocateRoomDto.getRoom_id());
+            long id=generateID();
+            alloc1.setAlloc_id(String.valueOf(id));
+            allocateroom_repo.save(alloc1);
+            allocateroom_repo.updateRoom(allocateRoomDto.getRoom_id(),0);
+            return "Allocation successful";
+
+        }
+        else{
+            return null;
+        }
+
+    }
+
+    public String deallocateRoom(DeallocateRoom_Dto deallocateRoomDto){
+
+        String room_id= deallocateRoomDto.getRoom_id();
+        //We have to check whether the admin is adding or someone else is adding the data
+        Allocation alloc=allocateroom_repo.getExistingAllocation(room_id,"1");
+        if(alloc!=null){
+            //System.out.println(room);
+            allocateroom_repo.updateRoom(deallocateRoomDto.getRoom_id(),1);
+            return "Room deallocation successful";
+        }
+        else{
+            return "Room is already free";
+        }
+    }
+
+    public List<Rooms> getRooms(int free){
+
+        List<Rooms> roomsList=getRooms_repo.getRooms(free);
+        return roomsList;
+    }
+
+    public List<Allocation> getAllocations(){
+
+        List<Allocation> allocationList=allocateroom_repo.getAllocations();
+        return allocationList;
+    }
+
+    public List<Test> getPositiveStudents(){
+
+        String result="+VE";
+        List<Test> positiveStudentList=addtest_repo.getPositives(result);
+        return positiveStudentList;
+    }
+}
